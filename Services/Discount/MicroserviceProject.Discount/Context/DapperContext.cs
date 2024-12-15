ce410556f8dd8170f171e557664cbf7a92658ea7
Melike İsbir <melikeisbr@gmail.com>
2024-12-15 19:50:53 +0300

    discount microservice, discount coupon definition service

---
+++ Services/Discount/MicroserviceProject.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using MicroserviceProject.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace MicroserviceProject.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MELI\\SQLEXPRESS; initial Catalog = MicroserviceProjectDiscountDb; integrated Security= true");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString); //bu metod çağrıldığında yeni bir sql bağlantısı oluşturma
+    }
+}
---
+++ Services/Discount/MicroserviceProject.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using MicroserviceProject.Discount.Dtos;
+
+namespace MicroserviceProject.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+    }
+}
