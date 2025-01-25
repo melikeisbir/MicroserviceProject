bbcb9cd7b2919d658640b236e739c06061849566
Melike İsbir <melikeisbr@gmail.com>
2025-01-25 21:28:05 +0300

    adding a template, layout adjustments

---
+++ Frontends/MicroserviceProject.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MicroserviceProject.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
