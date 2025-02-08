a3229c17c43cd4078c9e1456ff78a83152a6f745
Melike İsbir <melikeisbr@gmail.com>
2025-02-08 19:30:01 +0300

    update

---
+++ Frontends/MicroserviceProject.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
