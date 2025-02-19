6b34c60761b65d879e13dcd6006a50a8f19e8cb1
Melike İsbir <melikeisbr@gmail.com>
2025-02-19 22:26:16 +0300

    update

---
+++ Frontends/MicroserviceProject.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MicroserviceProject.WebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
