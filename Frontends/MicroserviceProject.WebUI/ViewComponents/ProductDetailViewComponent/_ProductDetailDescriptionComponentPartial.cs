bfeffdd6c1b01c7d59431680787f65781afe06c8
Melike İsbir <melikeisbr@gmail.com>
2025-02-24 20:59:51 +0300

    update

---
+++ Frontends/MicroserviceProject.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailDescriptionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailDescriptionComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MicroserviceProject.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailReviewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailReviewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
