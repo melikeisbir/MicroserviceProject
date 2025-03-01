b270860277b43ba79058779a2e7b597a45f6fcb2
Melike İsbir <melikeisbr@gmail.com>
2025-03-01 14:20:11 +0300

    shopping cart

---
+++ Frontends/MicroserviceProject.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartDiscountCouponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartDiscountCouponComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
