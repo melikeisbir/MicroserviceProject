c75ebf3057d9873f9018dcc34d0c9f56a4329941
Melike İsbir <melikeisbr@gmail.com>
2025-03-10 14:53:33 +0300

    dto layer

---
+++ Frontends/MicroserviceProject.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutMainSectionViewbagComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceProject.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutMainSectionViewbagComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
