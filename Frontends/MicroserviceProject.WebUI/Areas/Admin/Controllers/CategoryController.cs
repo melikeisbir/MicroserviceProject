d4f09b0f3ce7485b52f2168fcb474ed6468955c9
Melike İsbir <melikeisbr@gmail.com>
2025-04-16 20:42:29 +0300

    deleting and updating a category

--- Frontends/MicroserviceProject.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MicroserviceProject.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [AllowAnonymous]
+    [Route("Admin/Category")]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -15,6 +16,7 @@ namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -34,15 +36,17 @@ namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
             return View();
         }
         [HttpGet]
+        [Route("CreateCategory")]
         public IActionResult CreateCategory()
         {
             ViewBag.v1 = "Ana Sayfa";
             ViewBag.v2 = "Kategoriler";
-            ViewBag.v3 = "eni Kategori Girişi";
+            ViewBag.v3 = "Yeni Kategori Girişi";
             ViewBag.v0 = "Kategori İşlemleri";
             return View();
         }
         [HttpPost]
+        [Route("CreateCategory")]
         public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
         {
             var client = _httpClientFactory.CreateClient();
@@ -52,6 +56,51 @@ namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
             if(responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "Category", new {area="Admin"});
+            }
+            return View();
+        }
+
+        [Route("DeleteCategory/{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7056/api/Categories?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfası";
+            ViewBag.v0 = "Kategori İşlemleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7056/api/Categories/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7056/api/Categories/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
             }
             return View();
         }
