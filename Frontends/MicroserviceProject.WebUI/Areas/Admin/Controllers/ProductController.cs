dc8e6e1fb531df5e921aab44814bce9b714fc624
Melike İsbir <melikeisbr@gmail.com>
2025-04-22 18:54:08 +0300

    adding a product

--- Frontends/MicroserviceProject.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MicroserviceProject.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using MicroserviceProject.DtoLayer.CatalogDtos.ProductDtos;
+﻿using MicroserviceProject.DtoLayer.CatalogDtos.CategoryDtos;
+using MicroserviceProject.DtoLayer.CatalogDtos.ProductDtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
@@ -33,6 +35,42 @@ namespace MicroserviceProject.WebUI.Areas.Admin.Controllers
                 return View(values);
             }
 
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün Listesi";
+            ViewBag.v0 = "Ürün İşlemleri";
+            var client= _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7056/api/Categories");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> categoryValues = (from x in values
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryId
+                                                   }).ToList();
+            ViewBag.CategoryValues = categoryValues; 
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7056/api/Products", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
             return View();
         }
     }
